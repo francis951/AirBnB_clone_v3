dcc548706e00a98c706d187a7b3776f428718dc6
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-05-02 20:10:11 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-''' index'''
+"""
+index
+"""
 
-from flask import Flask
-from flask import Flask, abort
+from flask import jsonify
 from api.v1.views import app_views
-from os import name
-from models.state import State
-from flask import request
+
+from models import storage
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
