17aad5190942cfd1bd5d0e561cf08f2c05f436aa
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-05-02 20:21:55 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
+#!/usr/bin/python3
+"""
+Amenity Class from Models Module
+"""
+import os
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import backref
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class Amenity(BaseModel, Base):
