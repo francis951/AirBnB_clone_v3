e6fbf50cdb5fa6961cc9bd0e1fbefde818d656bb
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-05-02 19:26:14 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,51 +2,17 @@
 """Flask server (variable app)
 """
 
-
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
 from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown function
-    """
-    storage.close()
-
-
-@app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-    :return: returns 404 json
-    """
-    data = {
-        "error": "Not found"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 404
-
-    return(resp)
-
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
-
-
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000)) 
     app.run(host=host, port=port, threaded=True)
