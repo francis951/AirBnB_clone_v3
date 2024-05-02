3656203f9ed8e8401f33c25a71feaec6eb99eb57
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-05-02 19:39:06 +0300

    Update amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+''' new view for amenities'''
 
+from flask import Flask
+from flask import Flask, abort
+from api.v1.views import app_views
+from os import name
+from models.state import State
+from flask import request
 
 
 @app_views.route("/amenities", methods=["GET"], strict_slashes=False)
