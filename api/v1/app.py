26274dd297d470026cbe231252114eb861a92123
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-05-02 19:17:07 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,20 +9,37 @@ from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def downtear(self):
-    '''Status of your API'''
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify('error='Not found'), 404
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
 
 
 if __name__ == "__main__":
